e135e0c3bf4380e405b009cb1d09459ede5f09f1
Bill <747277763@qq.com>
2024-02-23 18:33:33 +0800

    修改注册API的成功返回值

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,6 +1,6 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
-import {errorHandler} from "../utils/error.js"
+import { errorHandler } from "../utils/error.js";
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   if (
@@ -12,7 +12,7 @@ export const signup = async (req, res, next) => {
     password === ""
   ) {
     // return res.status(400).json({ message: "All fields are required" });
-    next(errorHandler(400,"All fields are required"))
+    next(errorHandler(400, "All fields are required"));
   }
   //password: 要哈希的原始密码。
   //10:哈希的成本因子（也称为“盐”）
@@ -20,7 +20,11 @@ export const signup = async (req, res, next) => {
   const newUser = new User({ username, email, password: hashPassword });
   try {
     await newUser.save();
-    res.json({ message: "signup!" });
+    res.status(200).json({
+      statusCode: 200,
+      message: "Signup Success",
+      success: true,
+    });
   } catch (error) {
     next(error);
   }
