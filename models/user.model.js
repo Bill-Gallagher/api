022d2875f240d3d31deaade4840459ec8a7f74d1
Bill <747277763@qq.com>
2024-02-23 11:30:47 +0800

    新增用户mongoose模型

---
+++ models/user.model.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+// 定义一个模式
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      require: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      require: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      require: true,
+    },
+  },
+  { timestamps: true }
+);
+
+// 创建 User 模型
+const User = mongoose.model("User", userSchema);
+export default User;
