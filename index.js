00eec066a5aba511870a1992bfcd38f5bb0b24a7
Bill <747277763@qq.com>
2024-02-23 12:45:22 +0800

    创建signup api 路由

--- index.js
+++ index.js
@@ -2,19 +2,27 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
-console.log("test");
 mongoose
   .connect(process.env.MONGO)
   .then(() => console.log("Connected!"))
   .catch((err) => {
     console.log(err);
   });
+
+
+
 const app = express();
+// express.json() 中间件会解析传入请求的 JSON 数据，然后将其转换成 JavaScript 对象
+app.use(express.json());
+// 使用 express.urlencoded() 中间件来解析 x-www-form-urlencoded 格式的请求体
+app.use(express.urlencoded({ extended: true }));
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
 
 app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
