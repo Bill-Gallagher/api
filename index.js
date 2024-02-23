b43266e66480cfca67b047ede7be2a218059d067
Bill <747277763@qq.com>
2024-02-23 11:39:57 +0800

    测试

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 import dotenv from 'dotenv'
 
 dotenv.config()
-
+console.log("test")
 mongoose
   .connect(process.env.MONGO)
   .then(() => console.log("Connected!"))
