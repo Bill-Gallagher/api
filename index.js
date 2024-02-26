709c92ddb1bdc60845d4df2d43663531e3cfc2e8
Bill <747277763@qq.com>
2024-02-26 15:36:03 +0800

    使用cookieparser进行cookie解析，拓展http请求主体大小，能通过base64向后台传递图片

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 mongoose
@@ -13,10 +14,15 @@ mongoose
   });
 
 const app = express();
+// 设置 JSON 请求主体的最大大小为10MB（根据你的需求调整）
+app.use(express.json({ limit: '10mb' }));
+
 // express.json() 中间件会解析传入请求的 JSON 数据，然后将其转换成 JavaScript 对象
 app.use(express.json());
 // 使用 express.urlencoded() 中间件来解析 x-www-form-urlencoded 格式的请求体
 app.use(express.urlencoded({ extended: true }));
+// 使用cookie-parser中间件
+app.use(cookieParser());
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
