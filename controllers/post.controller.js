60818871a05c458a5f835196471398ad6eaa842b
Bill <747277763@qq.com>
2024-02-28 14:42:38 +0800

    创建post路由和Post数据结构

---
+++ controllers/post.controller.js
@@ -0,0 +1,24 @@
+import { errorHandler } from "../utils/error.js";
+import Post from "../models/post.model.js";
+export const create = async (req, res, next) => {
+  if (!req.body.title || !req.body.content) {
+    return next(errorHandler("Please provide all required fields"));
+  }
+  //   得到的 slug 就是一个 URL-friendly 的字符串，适合在 URL 中使用，因为它只包含小写字母、数字和连字符，且单词之间用连字符分隔。
+  const slug = req.body.title
+    .split(" ")
+    .join("-")
+    .toLowerCase()
+    .replace(/[^a-zA-Z0-9-]/g, "");
+  const newPost = new Post({
+    ...req.body,
+    slug,
+    userId: req.user.id,
+  });
+  try {
+    const savedPost = await newPost.save();
+    res.status(201).json(savedPost);
+  } catch (error) {
+    next(error);
+  }
+};
