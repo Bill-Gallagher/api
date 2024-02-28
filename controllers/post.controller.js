5d1204ef597cfe5eaea13672524a765debf49162
Bill <747277763@qq.com>
2024-02-28 14:51:00 +0800

    处理不能正常返回错误信息的bug

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -2,7 +2,7 @@ import { errorHandler } from "../utils/error.js";
 import Post from "../models/post.model.js";
 export const create = async (req, res, next) => {
   if (!req.body.title || !req.body.content) {
-    return next(errorHandler("Please provide all required fields"));
+    return next(errorHandler(500,"Please provide all required fields"));
   }
   //   得到的 slug 就是一个 URL-friendly 的字符串，适合在 URL 中使用，因为它只包含小写字母、数字和连字符，且单词之间用连字符分隔。
   const slug = req.body.title
