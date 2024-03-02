8a129c748bee495bf049c40c4e05d27a202bd8da
Bill <747277763@qq.com>
2024-03-02 12:58:49 +0800

    封装喜欢点赞路由

--- controllers/comment.controller.js
+++ controllers/comment.controller.js
@@ -34,3 +34,26 @@ export const getPostComments = async (req, res, next) => {
     next(error);
   }
 };
+
+export const likeComment = async (req, res, next) => {
+  try {
+    //根据评论号码，查询评论实体
+    const comment = await Comment.findById(req.params.commentId);
+    if (!comment) {
+      next(errorHandler(404, "Comment not found"));
+    }
+    //获取评论的liks数组
+    const userIndex = comment.likes.indexOf(req.user.id);
+    if (userIndex === -1) {
+      comment.numberOfLikes += 1;
+      comment.likes.push(req.user.id);
+    } else {
+      comment.numberOfLikes -= 1;
+      comment.likes.splice(userIndex, 1);
+    }
+    await comment.save();
+    res.status(200).json(comment);
+  } catch (error) {
+    next(error);
+  }
+};
