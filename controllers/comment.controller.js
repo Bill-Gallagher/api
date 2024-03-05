ffa525ef47fd9fca90ae2706bb79ab3abbb56844
Bill <747277763@qq.com>
2024-03-05 12:51:51 +0800

    评论模块添加编辑路由

--- controllers/comment.controller.js
+++ controllers/comment.controller.js
@@ -57,3 +57,30 @@ export const likeComment = async (req, res, next) => {
     next(error);
   }
 };
+
+//编辑评论
+export const editComment = async (req, res, next) => {
+
+  try {
+    //根据评论号码，查询评论实体
+    const comment = await Comment.findById(req.params.commentId);
+    // console.log(comment)
+    if (!comment) {
+      next(errorHandler(404, "Comment not found"));
+    }
+    if (comment.userId !== req.user.id) {
+      next(errorHandler(403, "You are not allowed to edit this comment"));
+    }
+    const editComment = await Comment.findByIdAndUpdate(
+      req.params.commentId,
+      {
+        content: req.body.content,
+      },
+      { new: true } //选项表示返回更新后的文档而不是原始文档。
+    );
+    console.log(editComment,222)
+    res.status(200).json(editComment);
+  } catch (error) {
+    next(error);
+  }
+};
